85119738959a0d313d8339a6f29aa2b5ee49d583
sRabosio <rabosiosimone@gmail.com>
2022-05-23 15:00:52 +0200

    added commandHandler to clientHandler to execute commands

--- src/main/java/org/example/ClientHandler.java
+++ src/main/java/org/example/ClientHandler.java
@@ -4,6 +4,7 @@ import java.net.Socket;
 
 public class ClientHandler implements Runnable{
     private TcpConnection conn;
+    private final CommandHandler cmdh = new CommandHandler();
 
     public ClientHandler(Socket sock){
         System.out.println("--conneting to "+sock.getRemoteSocketAddress() + "...");
@@ -14,7 +15,9 @@ public class ClientHandler implements Runnable{
     @Override
     public void run() {
             for(String s; (s = conn.read()) != null;) {
-                System.out.println(s);
+                conn.write(
+                        cmdh.execute(s+"\n")
+                );
             }
     }
 }
