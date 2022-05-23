30d0d33a030f026a33037494319223f373f177fd
sRabosio <rabosiosimone@gmail.com>
2022-05-23 14:29:05 +0200

    Added TcpConnection

---
+++ src/main/java/org/example/TcpConnection.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class TcpConnection {
+    private Socket socket;
+    private PrintWriter writer;
+    private BufferedReader reader;
+
+    public TcpConnection(Socket socket){
+        setSocket(socket);
+    }
+
+    //reads string from the other side, returns null upon failure
+    public String read(){
+        String result = null;
+
+        try {
+            result = reader.readLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+
+    //writes string to the other side
+    public void write(String msg){
+        writer.write(msg);
+    }
+
+    private void initWriter(){
+        try {
+            writer = new PrintWriter(
+                    socket.getOutputStream()
+            );
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void initReader(){
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        if(socket == null) return;
+        this.socket = socket;
+        initReader();
+        initWriter();
+    }
+}
