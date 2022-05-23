ab474e7f904b038ed59e43ce04a9a4cc493b41bf
sRabosio <rabosiosimone@gmail.com>
2022-05-23 14:41:43 +0200

    added waitClients method to server

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import com.sun.security.ntlm.Client;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 
@@ -10,12 +12,26 @@ public class Server {
     private static ServerSocket server;
 
     public static void main(String[] args){
-
+        initServer();
+        waitClients();
     }
 
     private static void initServer(){
         try {
             server = new ServerSocket(port);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            exit(-1);
+        }
+        System.out.println("--server started");
+    }
+
+    private static void waitClients(){
+        System.out.println("--waiting clients");
+        try {
+            new Thread(new ClientHandler(
+                    server.accept()
+            ));
         } catch (IOException e) {
             System.out.println(e.getMessage());
             exit(-1);
